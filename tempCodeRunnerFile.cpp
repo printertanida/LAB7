f4f39737d5b370311dd6dda150a1c1a2504446cc
Tanida <printer.rung@gmail.com>
2024-12-06 15:55:56 +0700

    lab7_3.cpp completed

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,15 +2,7 @@
 using namespace std;
 
 char before(char x){
-	if (x >= 'A' && x <= 'Z') {
-        if (x == 'A') {
-            return 'Z';
-        } else {
-            return x - 1;
-        }
-    } else {
-        return '0';
-    }
+	//Write your function definition here
 }
 
 int main(){
